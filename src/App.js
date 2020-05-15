5917c78c1948b2e4076de77af3daf4c565f27263
Alexander <alexcrisel4@gmail.com>
2020-05-15 13:54:46 -0700

    Adds nav bar

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import './App.css';
 import Footer from './components/Footer'
 import Header from './components/Header'
+import Navbar from './components/Navbar'
 import MainContent from './components/MainContent'
+
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Navbar/>
       <MainContent/>
       <Footer/>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../logo.svg';
+
 export default function Header() {
 return(<header className="App-header">
   {/* <img src={logo}></img> */}
