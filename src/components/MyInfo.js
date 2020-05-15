bfb67de1956fc021f0aa6111e357baa270ce823a
Alexander <alexcrisel4@gmail.com>
2020-05-15 12:13:40 -0700

    creates MyInfo component

---
+++ src/components/MyInfo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+function MyInfo() {
+  return (
+    <div>
+      <h1>Alexander</h1>
+      <p>
+        Here's some info about me! 
+      </p>
+   
+    <ul>
+      <li>Island 1</li>
+      <li>Island 2</li>
+      <li>Island 3</li>
+    </ul>
+    </div>
+  )
+}
+
+export default MyInfo
--- src/index.js
+++ src/index.js
@@ -3,19 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import MyInfo from './components/MyInfo'
 
 
-let list = <ul>
-  <li>Item 1</li>
-  <li>Item 2</li>
-  <li>Item 3</li>
-</ul>
+
+
 ReactDOM.render(
 
   // <React.StrictMode>
   //   <App />
   // </React.StrictMode>,
-  list ,document.getElementById('root')
+  <MyInfo/> ,document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
